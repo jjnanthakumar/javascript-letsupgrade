63cf9558dce7688688c09e736b112dba3c7004f0
Nanthakumar J J <46053893+jjnanthakumar@users.noreply.github.com>
2021-09-10 11:14:37 +0530

    Assignment 3 Added

--- solution.js
+++ solution.js
@@ -17,16 +17,44 @@ console.log(Array.from(Array(10).keys()).filter(x => x % 2 == 0))
 
 // With loop
 let arr = Array.from(Array(10).keys())
-for(let i=0;i<arr.length;i++){
-    arr[i]%2==0 && console.log(arr[i])
+for (let i = 0; i < arr.length; i++) {
+    arr[i] % 2 == 0 && console.log(arr[i])
 }
 
 // Create a array with five city names and print it in reverse order
-let city_names = ['Madurai','Chennai','Trichy','Coimbatore','Salem']
+let city_names = ['Madurai', 'Chennai', 'Trichy', 'Coimbatore', 'Salem']
 // One liner
 console.log([...city_names].reverse())
 
 // With loop
-for(let i=city_names.length-1;i>0;i--){
+for (let i = city_names.length - 1; i > 0; i--) {
     console.log(city_names[i])
+}
+
+var Id = 0;
+function addTask() {
+    let data = document.getElementById('inp').value;
+    document.getElementById('two').innerHTML += `
+    <span id='${Id}'>
+    <h1 style="display: inline-block !important" class='todo'>${data}</h1>
+    <button class='btn btn-sm btn-danger' onclick="deleteTask(${Id})"><i class='fa fa-trash'></i></button>
+    <button class='btn btn-sm btn-primary' onclick="updateTask(${Id})"><i class='fa fa-edit'></i></button>
+    </span><br/>
+    `;
+    document.getElementById('inp').value = "";
+}
+
+function deleteTask(id) {
+    document.getElementById(id).remove()
+}
+
+function updateTask(id) {
+    let data = document.getElementById(id).innerText
+    document.getElementById('inp').value = data
+    deleteTask(id)
+}
+function changeColor() {
+
+    document.getElementById('two').style.backgroundColor = "lightblue";
+
 }
